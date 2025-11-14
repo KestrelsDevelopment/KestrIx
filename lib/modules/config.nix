ab9f383bac386c0f62c5ce5e63116ac958bb385f
AceOfKestrels <git@kestrels.dev>
2025-11-14 13:12:09 +0100

    Fix modules paths

--- lib/modules/config.nix
+++ lib/modules/config.nix
@@ -46,8 +46,8 @@ in
                     (flake + "/hardware.nix")
                     (flake + "/state.nix")
                     inputs.home-manager.nixosModules.home-manager
-                    ../options/options.nix
-                    ../overlays/overlays.nix
+                    ../../options/options.nix
+                    ../../overlays/overlays.nix
                     {
                         environment.variables.FLAKE_PATH = lib.mkDefault src;
                         networking.hostName = lib.mkForce hostname;
