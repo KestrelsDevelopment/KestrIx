f95adb89331bf587e36afbb954ce7337e358ed42
AceOfKestrels <git@kestrels.dev>
2026-08-01 14:09:30 +0200

    fix gitbutler package

--- overlays/overlays.nix
+++ overlays/overlays.nix
@@ -1,7 +1,6 @@
 {
     nixpkgs.overlays = [
-        # (import ./pkgs/freelens.nix)
-        # (import ./pkgs/gitbutler-bin.nix)
+        (import ./pkgs/gitbutler-bin.nix)
         (import ./pkgs/convergence-mod-launcher.nix)
     ];
 }
--- overlays/pkgs/gitbutler-bin.nix
+++ overlays/pkgs/gitbutler-bin.nix
@@ -1,7 +1,7 @@
 self: super:
 
 let
-    pname = "gitbutler";
+    pname = "gitbutler-bin";
     version = "0.16.10";
     revision = "2482";
 
@@ -10,27 +10,17 @@ let
         sha256 = "01marw05sp9p5z1kgi6ysnhnhcv9w5jx2qfjjznj54wcpfgxm0n9";
     };
 
-    appImage = "${srcArchive}/GitButler_${version}_amd64.AppImage";
+    src = "${srcArchive}/GitButler_${version}_amd64.AppImage";
 in
 {
     gitbutler-bin = super.appimageTools.wrapType2 {
-        inherit pname version;
-        src = appImage;
+        inherit pname src;
 
-        extraInstallCommands = ''
-            for f in $out/share/applications/*.desktop; do
-              [ -e "$f" ] || continue
-              substituteInPlace "$f" \
-                --replace-fail 'Exec=AppRun' 'Exec=${pname}'
-            done
-        '';
+        version = "${version}-${revision}";
 
-        meta = with super.lib; {
-            description = "Git client for simultaneous branches";
-            homepage = "https://gitbutler.com";
-            license = licenses.fsl11Mit;
-            mainProgram = pname;
-            platforms = [ "x86_64-linux" ];
-        };
+        # extraPkgs =
+        #     pkgs: with pkgs; [
+        #         icu
+        #     ];
     };
 }
