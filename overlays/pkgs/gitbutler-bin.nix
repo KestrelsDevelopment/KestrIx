3432af64d55553b1fff858ec02a440626fcaf353
AceOfKestrels <git@kestrels.dev>
2026-08-01 15:28:46 +0200

    add desktop icon

--- overlays/pkgs/gitbutler-bin.nix
+++ overlays/pkgs/gitbutler-bin.nix
@@ -11,6 +11,11 @@ let
     };
 
     src = "${srcArchive}/GitButler_${version}_amd64.AppImage";
+
+    icon = super.fetchurl {
+        url = "https://camo.githubusercontent.com/9c6adb93179b0f1f14ddb23ebe36247b56d9b707db9ea1946b77d5476983a7d8/68747470733a2f2f6769746275746c65722d646f63732d696d616765732d7075626c69632e73332e75732d656173742d312e616d617a6f6e6177732e636f6d2f6d642d6c6f676f2e706e67";
+        sha256 = "0dwv8n1rdyimwxqm8x100r4sdxsa1hrjw13s4jh9c3wbh9ykdwbk";
+    };
 in
 {
     gitbutler-bin = super.appimageTools.wrapType2 {
@@ -26,16 +31,18 @@ in
             (super.makeDesktopItem {
                 name = "GitButler";
                 exec = "gitbutler-bin %U";
-                desktopName = "gitbutler";
-                comment = "GitButler";
-                categories = [ "Graphics" ];
+                icon = "gitbutler";
+                desktopName = "GitButler";
+                comment = "Git client";
+                categories = [ "Utilities" ];
                 startupWMClass = "gitbutler";
             })
         ];
 
         extraInstallCommands = ''
-            # Force copyDesktopItems to run inside wrapType2's custom build script
             copyDesktopItems
+
+            install -m 444 -D ${icon} $out/share/icons/hicolor/512x512/apps/gitbutler.png
         '';
     };
 }
