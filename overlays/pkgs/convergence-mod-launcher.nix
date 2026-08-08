c526439d83f5964a323c7cca689ce3bda947933e
Kessandra M. Winkelnkemper <git@kestrels.dev>
2026-08-08 21:51:29 +0200

    [pkgs] add gitbutler-bin and convergence-mod-launcher (#7)
    * add convergence-mod-launcher package
    
    * fix gitbutler package
    
    * fix gitbutler hash
    
    * fix gitbutler hash again
    
    * add desktop item
    
    * fix desktop item
    
    * fix desktop item again
    
    * add desktop icon
    
    * fix category name
    
    * trace flake
    
    * remove trace
    
    * add desktop item to convergence
    
    * fix syntax error
    
    * fix overlay?
    
    * fix overlay again?
    
    * turn overlays into actual packages
    
    * fix config builder

---
+++ overlays/pkgs/convergence-mod-launcher.nix
@@ -0,0 +1,49 @@
+{
+    fetchurl,
+    appimageTools,
+    copyDesktopItems,
+    makeDesktopItem,
+    ...
+}:
+
+let
+    pname = "convergence-mod-launcher";
+    version = "1.0.3.1";
+
+    src = fetchurl {
+        url = "https://github.com/The-Convergence-Team/ConvergenceER-Public/releases/download/v${version}/ConvergenceLauncher_linux.AppImage";
+        sha256 = "0r6ghqkbr62qicp5dq9hqrlkjbk9g5jq065fanlaqi27ixkjnnfq";
+    };
+
+    icon = ../assets/convergence-mod-launcher.png;
+in
+appimageTools.wrapType2 {
+    inherit version pname src;
+
+    extraPkgs =
+        pkgs: with pkgs; [
+            icu
+        ];
+
+    nativeBuildInputs = [
+        copyDesktopItems
+    ];
+
+    desktopItems = [
+        (makeDesktopItem {
+            name = "ELDEN RING - The Convergence";
+            exec = "convergence-mod-launcher %U";
+            icon = "convergence-mod-launcher";
+            desktopName = "ELDEN RING - The Convergence";
+            comment = "The Convergence mod for ELDEN RING";
+            categories = [ "Game" ];
+            startupWMClass = "convergence-mod-launcher";
+        })
+    ];
+
+    extraInstallCommands = ''
+        copyDesktopItems
+
+        install -m 444 -D ${icon} $out/share/icons/hicolor/512x512/apps/convergence-mod-launcher.png
+    '';
+}
