53aecee26d0dffa00cafc85c0f69f1628af7a4c0
AceOfKestrels <git@kestrels.dev>
2026-08-08 20:48:29 +0200

    add desktop item to convergence

--- overlays/pkgs/convergence-mod-launcher.nix
+++ overlays/pkgs/convergence-mod-launcher.nix
@@ -8,6 +8,8 @@ let
         url = "https://github.com/The-Convergence-Team/ConvergenceER-Public/releases/download/v${version}/ConvergenceLauncher_linux.AppImage";
         sha256 = "0r6ghqkbr62qicp5dq9hqrlkjbk9g5jq065fanlaqi27ixkjnnfq";
     };
+
+    icon = ../assets/convergence-mod-launcher.png
 in
 {
     convergence-mod-launcher = super.appimageTools.wrapType2 {
@@ -17,5 +19,27 @@ in
             pkgs: with pkgs; [
                 icu
             ];
+
+        nativeBuildInputs = [
+            super.copyDesktopItems
+        ];
+
+        desktopItems = [
+            (super.makeDesktopItem {
+                name = "ELDEN RING - The Convergence";
+                exec = "convergence-mod-launcher %U";
+                icon = "convergence-mod-launcher";
+                desktopName = "ELDEN RING - The Convergence";
+                comment = "The Convergence mod for ELDEN RING";
+                categories = [ "Gaming" ];
+                startupWMClass = "convergence-mod-launcher";
+            })
+        ];
+
+        extraInstallCommands = ''
+            copyDesktopItems
+
+            install -m 444 -D ${icon} $out/share/icons/hicolor/512x512/apps/convergence-mod-launcher.png
+        '';
     };
 }
